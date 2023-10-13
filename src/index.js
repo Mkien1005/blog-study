cf4c85f2b177deab6eb0cae3b8680ac43485f219
Mkien <manhkien1005@gmail.com>
2023-10-13 14:28:22 +0700

    thêm thư viện

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const handlebars = require("express-handlebars");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+app.use(morgan());
+//Template engine
+//dòng dưới nghĩa là: app đang sử dụng engine đặt tên là handlebars
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+//set view engine là handlebars
+app.set("view engine", "hbs");
+//set đường dẫn tới views
+app.set("views", path.join(__dirname, "resources/views"));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
