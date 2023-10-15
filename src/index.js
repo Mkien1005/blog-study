39970d5999b7bd7ba03b9ce345d5e573f9228ad9
Mkien <manhkien1005@gmail.com>
2023-10-15 17:56:18 +0700

    post method

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ const handlebars = require("express-handlebars");
 const morgan = require("morgan");
 const app = express();
 const port = 3000;
-app.use(morgan());
+// app.use(morgan());
+//đưa thư mục public lên có thể xem đc phía web
 app.use(express.static(path.join(__dirname, "public")));
+//dùng middleware xử lý việc lấy dữ liệu từ body trong form
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//.json() để lấy dữ liệu khi dùng fetch, XMLHttp, axios,...
+//urlencoded() để lấy dữ liệu khi dùng form
 //Template engine
 //dòng dưới nghĩa là: app đang sử dụng engine đặt tên là handlebars
 app.engine(
@@ -23,6 +29,13 @@ app.get("/", (req, res) => {
 });
 app.get("/news", (req, res) => {
   res.render("news");
+});
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  console.log(req.body);
+  res.send("");
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
